b6e9fb96c4de806c888c305041d947271f831f7a
daiyi <clsun91@gmail.com>
2015-04-27 15:43:10 -0700

    static and media paths in settings.py

--- hatch/settings.py
+++ hatch/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -131,6 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+# URL prefix for static files, eg "convox.org/hatch'/static/'"
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/srv/sites/hatch'
+# absolute path to static file directory collected by python manage.py collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# URL for media, eg "convox.org/hatch'/media'"
+MEDIA_URL = '/media/'
+
+# absolute path to user-uploaded files
+MEDIA_ROOT = '/srv/media/hatch_media'
