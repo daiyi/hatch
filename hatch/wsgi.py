7f94de2825ec07582e725dc9aac98ac5cd72edd8
daiyi <clsun91@gmail.com>
2015-05-03 16:53:00 -0700

    remove environ vars from wsgi and into wsgi_prod

--- hatch/wsgi.py
+++ hatch/wsgi.py
@@ -4,4 +4,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hatch.settings")
 
+# In production, fill in the actual values of these environ vars. 
+# In development, export these environ vars in ~/.bashrc
+os.environ["HATCH_SECRET_KEY"] = ""
+os.environ["HATCH_DB_PW"] = ""
+os.environ["HATCH_ACCOUNT_REDIRECT_URL"] = "" 
+os.environ["HATCH_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY="] = "" 
+os.environ["HATCH_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET"] = "" 
+
 application = get_wsgi_application()
