a21b1816dc8f3f6f1c95e38e3f8d0b4260e893ed
root <root@convox.org>
2015-05-06 23:08:52 -0700

    Added a signal reciever to create a users db objects on sign up

---
+++ incubator/signals.py
@@ -0,0 +1,17 @@
+from account.signals import user_signed_up
+from django.dispatch import receiver
+from incubator.models import Incubator, Egg
+
+from sys import stderr
+
+@receiver(user_signed_up)
+def new_account_handler(sender, **kwargs):
+	if not 'user' in kwargs:
+		sys.stderr.write("Newly created user not provided!")
+		return
+	user = kwargs['user']
+	incubator = Incubator(owner=user)
+	incubator.save()
+	
+	egg = Egg(incubator=incubator)
+	egg.save()
